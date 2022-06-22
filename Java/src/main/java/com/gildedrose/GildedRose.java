df053ee481c5bc221ed774d02a72c8d65f3a9281
Akrati Mishra <akrati.mishra@travelport.com>
2022-06-22 14:13:30 +0530

    Replaced for with foreach and removed all occurrences of Sulfuras from the Items array as no change is required for it.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,74 +1,73 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class GildedRose {
-    private static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
+	private static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
 	private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
 	private static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 	private static final String AGED_BRIE = "Aged Brie";
 	Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE)
-                    && !items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                    	if(items[i].name.equals(CONJURED_MANA_CAKE)){
-                    		items[i].quality = items[i].quality - 2;
-                    	} else{
-                    		items[i].quality = items[i].quality - 1;
-                    	}
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+	public void updateQuality() {
+		List<Item> itemsWithoutSulfuras = Arrays.asList(items).parallelStream()
+				.filter(item -> (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS))).collect(Collectors.toList());
+		for (Item item : itemsWithoutSulfuras) {
+			if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+				if (item.quality > 0) {
+					if (item.name.equals(CONJURED_MANA_CAKE)) {
+						item.quality = item.quality - 2;
+					} else {
+						item.quality = item.quality - 1;
+					}
+				}
+			} else {
+				if (item.quality < 50) {
+					item.quality = item.quality + 1;
 
-                    if (items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+					if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+						if (item.sellIn < 11) {
+							if (item.quality < 50) {
+								item.quality = item.quality + 1;
+							}
+						}
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+						if (item.sellIn < 6) {
+							if (item.quality < 50) {
+								item.quality = item.quality + 1;
+							}
+						}
+					}
+				}
+			}
 
-            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+			item.sellIn = item.sellIn - 1;
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE)) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                            	if(items[i].name.equals(CONJURED_MANA_CAKE)){
-                            		items[i].quality = items[i].quality - 2;
-                            	} else{
-                            		items[i].quality = items[i].quality - 1;
-                            	}
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
-    }
+			if (item.sellIn < 0) {
+				if (!item.name.equals(AGED_BRIE)) {
+					if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+						if (item.quality > 0) {
+							if (item.name.equals(CONJURED_MANA_CAKE)) {
+								item.quality = item.quality - 2;
+							} else {
+								item.quality = item.quality - 1;
+							}
+						}
+					} else {
+						item.quality = item.quality - item.quality;
+					}
+				} else {
+					if (item.quality < 50) {
+						item.quality = item.quality + 1;
+					}
+				}
+			}
+		}
+	}
 }
